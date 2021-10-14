b56125b880e9e3c7abbfc025855ef9ee59b376e8
Hoang Hoanh <hoanhqb1@gmail.com>
2021-10-14 15:03:51 +0700

    add edit search delete book

---
+++ resources/views/layout/booklayout.blade.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Book master</title>
+    <link href="{{ asset('css/book.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        @include('book')
+    </div>
+</body>
+</html>
