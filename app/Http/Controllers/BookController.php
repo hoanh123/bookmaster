83959fb775f92feeb0a9b4ea72611017dae4e750
Hoang Hoanh <hoanhqb1@gmail.com>
2021-10-18 20:18:09 +0700

    validation book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,71 +6,106 @@ use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use App\Http\Requests\BookRequest;
+use Validator;
 
 class BookController extends Controller
 {
-    public function index(Request $request)
+    public function home(Request $request)
+    {
+        return view('layout/booklayout');
+    }
+
+    public function index(BookRequest $request)
     {
         if ($request->has('search')) {
             $book = $this->searchBook($request);
-            $date = explode( '-', $book['publication_day'] );
-            $book['year'] = $date[0];
-            $book['month'] = $date[1];
-            $book['day'] = $date[2];
-            return view('layout/booklayout', $book);
         }
 
         if ($request->has('add')) {
             $book = $this->addBook($request);
-            return view('layout/booklayout');
         }
 
         if ($request->has('update')) {
             $book = $this->updateBook($request);
-            return view('layout/booklayout');
         }
 
         if ($request->has('delete')) {
             $book = $this->deleteBook($request);
-            return view('layout/booklayout');
         }
-
-        return view('layout/booklayout');
-    }
-
-    public function searchBook($request)
-    {
-        if ($request->has('book_id')) {
-            $bookid = $request->input('book_id');
-            $book = Book::searchBookById($bookid);
-        }
-        return $book;
+        return view('layout/booklayout', $book);
     }
 
     public function addBook($request)
     {
+        $data = array();
+        $book_id = $request->input('book_id');
         if ($request->has('book_id')) {
-            $bookid = $request->input('book_id');
             $book = Book::insertBook($request);
+            if($book === true) {
+                $data['message'] = "Đã thêm thành công book ID : " . $book_id;
+            } else if ($book == false){
+                $data['error_book_id'] = 'Thêm book ID : ' . $book_id . 'thất bại';
+            } else {
+                $data['error_system'] = 'Lỗi hệ thống';
+            }
         }
-        return $book;
+        return $data;
     }
 
     public function updateBook($request)
     {
+        $data = array();
+        $book_id = $request->input('book_id');
         if ($request->has('book_id')) {
-            $bookid = $request->input('book_id');
             $book = Book::editBookById($request);
+            if($book === true) {
+                $data['message'] = "Đã sửa thành công book ID : " . $book_id;
+            } else if ($book == false){
+                $data['error_book_id'] = 'Book ID : ' . $book_id . ' truyền vào không chính xác';
+            } else {
+                $data['error_system'] = 'Lỗi hệ thống';
+            }
         }
-        return $book;
+        return $data;
+    }
+
+    public function searchBook($request)
+    {
+        $data = array();
+        $book_id = $request->input('book_id');
+        if ($request->has('book_id')) {
+            $book = Book::searchBookById($book_id);
+            if($book == false) {
+                $data['error_book_id'] = 'Không tồn tại book ID : ' . $book_id;
+            } else if ($book == "error_system"){
+                $data['error_system'] = 'Lỗi hệ thống';
+            } else {
+                $data = $book;
+                $date = explode( '-', $book['publication_day'] );
+                $data['year'] = $date[0];
+                $data['month'] = $date[1];
+                $data['day'] = $date[2];
+                $data['message'] = "Đã tìm thấy book ID : " . $book_id;
+            }
+        }
+        return $data;
     }
 
     public function deleteBook($request)
     {
+        $data = array();
+        $book_id = $request->input('book_id');
         if ($request->has('book_id')) {
-            $bookid = $request->input('book_id');
-            $book = Book::deleteBookById($bookid);
+            $book = Book::deleteBookById($book_id);
+            if($book === true) {
+                $data['message'] = "Đã xóa book ID : " . $book_id . ' thành công';
+            } else if ($book == false){
+                $data['error_book_id'] = 'Không tồn tại book ID : ' . $book_id;
+            } else {
+                $data['error_system'] = 'Lỗi hệ thống';
+            }
         }
-        return $book;
+        return $data;
     }
 }
